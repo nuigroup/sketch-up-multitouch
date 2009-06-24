dcfb2aa1364cc77a77a8fbb2d16fa952f0787327
anirudhsharma.crypto@gmail.com <anirudhsharma.crypto@gmail.com@0b448cae-322c-11de-8e0c-53a27eea117e>
2009-06-24 14:21:21 +0000

    SkSocket Connection integration into tuioconn.rb experiment
    git-svn-id: http://sketchupmultitouch.googlecode.com/svn/trunk@24 0b448cae-322c-11de-8e0c-53a27eea117e

--- SketchUpMT/tuioconn.rb
+++ SketchUpMT/tuioconn.rb
@@ -1,9 +1,23 @@
+#$pf = Sketchup.find_support_file("Plugins")
 
-  # SKSocket.disconnect
-  # $out.flush
-  # $out.close
-   
- #  puts 'Receiving Data'
+
+#def start
+#  $out = File.open(File.join($pf, "tuio.out"), "w")
+ # SKSocket.connect("localhost", 3000)
+ # SKSocket.add_socket_listener do |s|
+  #  $out.print "#{Time.now.to_i}: "
+  #  $out.puts s
+  #  $out.flush
+ #   if s == "Connection established"
+ #   end
+ # end
+#end
+
+#def stop
+ # SKSocket.disconnect
+ # $out.flush
+ # $out.close
+#  $out = nil
 # end
    
 
