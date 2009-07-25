a048882747f9f7e892ec1df51b5680d578a0f613
anirudhsharma.crypto@gmail.com <anirudhsharma.crypto@gmail.com@0b448cae-322c-11de-8e0c-53a27eea117e>
2009-07-25 20:22:39 +0000

    Some code cleanups, and important additions to Ruby loader
    git-svn-id: http://sketchupmultitouch.googlecode.com/svn/trunk@43 0b448cae-322c-11de-8e0c-53a27eea117e

--- SketchUpMT.rb
+++ SketchUpMT.rb
@@ -13,15 +13,115 @@
         # ----------------------------------
         def initialize
             super();         # <== initialize the Ruby TuioClient extension
-        end
+            @aSketchUpRect = GetSketchUpRect();
+            @aSketchUpViewRect = GetSketchUpViewRect();
+            # show SketchUp coordinates in debugging log
+            msg = sprintf("SketchUpRect x[%d]y[%d]width[%d]height[%d]",
+                @aSketchUpRect[0],@aSketchUpRect[1],@aSketchUpRect[2],@aSketchUpRect[3]);
+            puts(msg);
+            Logit(msg);
+            msg = sprintf("SketchUpViewRect x[%d]y[%d]width[%d]height[%d]",
+                @aSketchUpViewRect[0],@aSketchUpViewRect[1],@aSketchUpViewRect[2],@aSketchUpViewRect[3]);
+            puts(msg);
+            Logit(msg);
+            @SuViewX = @aSketchUpViewRect[0];
+            @SuViewY = @aSketchUpViewRect[1];
+            @SuViewWidth = @aSketchUpViewRect[2];
+            @SuViewHeight = @aSketchUpViewRect[3];
+
+            @aperture = nil;
+            @model = Sketchup.active_model
+            @view = @model.active_view
+            @ph = @view.pick_helper
+            @ip = Sketchup::InputPoint.new
+            @ipXY = [0,0];
+       end
         # ----------------------------------
-        #	OnTuioData (called by TuioClient when TUIO data is available)
+        #       PickHelper
+        # ----------------------------------
+        # load the EventRelay Ruby extension
+
+        def phCursorDown(flags, x, y, view)
+            entity = entity_under_mouse(view, x, y)
+            @xdown = x
+            @ydown = y
+            if entity && selection_include?(entity)
+                @mode_erase = true
+            else
+                @mode_erase = false
+            end
+        end
+
+        #Identify the entity under the mouse
+        def entity_under_mouse(view, x, y)
+            (@aperture) ? @ph.do_pick(x, y, @aperture) : @ph.do_pick(x, y)
+            entity = @ph.best_picked
+            @entity = nil
+            if entity
+                @ip.pick view, x, y
+                @ipXY = [x,y];
+                @entity = entity
+                puts "entity: #{@entity}"
+            end
+        end
+
+        #Move halfway to point
+        def move_halfway_toward(entity, target_pt)
+            begin
+                current_pt = entity.transformation.origin;
+                halfway = Geom::Point3d.linear_combination(0.5, current_pt, 0.5,
+                    target_pt);
+                # Undo our current transform with inverse,
+                # then move to the halfway point.
+                entity.transform! entity.transformation.inverse;
+                entity.transform! Geom::Transformation.new halfway;
+                # Invalidate our view to force SU to refresh the screen.
+                Sketchup.active_model.active_view.invalidate;
+            rescue Exception => e
+                puts e.message
+                e.backtrace.each{|line| puts "\t" + line }
+            end;
+        end
+
+        #Move entity to point
+        def move_toward(entity, target_pt)
+            begin
+                current_pt = entity.transformation.origin;
+                halfway = Geom::Point3d.linear_combination(1.0, current_pt, 1.0,
+                    target_pt)
+                # Undo our current transform with inverse,
+                # then move to the halfway point.
+                entity.transform! entity.transformation.inverse
+                entity.transform! Geom::Transformation.new halfway
+                # Invalidate our view to force SU to refresh the screen.
+                Sketchup.active_model.active_view.invalidate
+            rescue Exception => e
+                puts e.message
+                e.backtrace.each{|line| puts "\t" + line }
+            end;
+        end
+
+        # ----------------------------------
+        #       OnTuioData (called by TuioClient when TUIO data is available)
         # ----------------------------------
         #def OnTuioData( stringOfTuioData )
             ##Logit "FROM RUBY OnTuioData: #{stringOfTuioData}"
             #puts stringOfTuioData; #write to SketchUp console
             #return true
         #end
+
+        # Convert TUIO to SketchUp view coordinates
+        def GetSuViewXY(tuiox, tuioy)
+            #Updatae SketchUp View rect in case user resized it.
+            @aSketchUpViewRect = GetSketchUpViewRect();
+            @SuViewX = @aSketchUpViewRect[0];
+            @SuViewY = @aSketchUpViewRect[1];
+            @SuViewWidth = @aSketchUpViewRect[2];
+            @SuViewHeight = @aSketchUpViewRect[3];
+            x = Integer(@SuViewWidth * tuiox);
+            y = Integer(@SuViewHeight * tuioy);
+            return [x,y];
+        end
 
         def OnTuioAddObject( symbolID, sessionID, positionX, positionY, angle)
             puts "AddObject: #{symbolID} #{sessionID} #{positionX} #{positionY} #{angle}"
@@ -41,11 +141,34 @@
 
         def OnTuioAddCursor( symbolID, sessionID, positionX, positionY )
             puts "AddCursor: #{symbolID} #{sessionID} #{positionX} #{positionY}"
+            x,y = GetSuViewXY(positionX,positionY);
+            msg = sprintf("ViewX[%d] ViewY[%d]", x, y);
+            puts msg;
+            flags = 0;
+            phCursorDown(flags, x, y, @view);
             return true
         end
 
         def OnTuioSetCursor( symbolID, sessionID, positionX, positionY, motionSpeed, motionAccel )
-            puts "SetCursor: #{symbolID} #{sessionID} #{positionX} #{positionY} #{motionSpeed} #{motionAccel}"
+            #puts "SetCursor: #{symbolID} #{sessionID} #{positionX} #{positionY} #{motionSpeed} #{motionAccel}"
+            x,y = GetSuViewXY(positionX,positionY);
+            msg = sprintf("ViewX[%d] ViewY[%d]", x, y);
+            puts msg;
+            # move the chosen entity to the new coordinates
+            if (@entity)
+                xDelta = x-@ipXY[0];
+                yDelta = y-@ipXY[1];
+                #moveto_pt = Geom::Point3d.new( xDelta, yDelta, 0);
+                #move_halfway_toward(@entity, moveto_pt)
+                msg = sprintf("Deltas[%d,%d] ipXY[%d,%d]", xDelta, yDelta, @ipXY[0],@ipXY[1]);
+                puts(msg);
+                #if (xDelta): xDelta = (xDelta<0)? -1:1; end;
+                #if (yDelta): yDelta = (yDelta<0)? -1:1; end;
+                moveto_pt = Geom::Point3d.new( xDelta, yDelta, 0);
+                move_toward(@entity, moveto_pt)
+                @ipXY[0] = x;
+                @ipXY[1] = y;
+            end;
             return true
         end
 
